cd57b8de761b3985e8859e14fbc2d37695751efc
Ortega <ortega.victor@gmail.com>
2020-08-14 10:05:37 +0200

    Add weather one day and daily city

---
+++ js/info.js
@@ -0,0 +1,100 @@
+//calling the API
+// let myInit = {
+//     method: "GET",
+//     mode: "cors",
+//     cache: "default",
+// };
+let cityName = localStorage.getItem("name_localCity");
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e4dfa41e22f93aa72d2e80838a3bb930`
+
+
+var cityWeatherInfo = {};
+var cityWeatherInfoDaily = [];
+var dateNow = new Date();
+
+
+const getWeatherData = async () => {
+
+    const res = await fetch(url);
+    cityWeatherInfo = await res.json();
+
+    // let url2=`http://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&
+    // exclude=hourly,daily&appid={YOUR API KEY}`
+    let url2 = `http://api.openweathermap.org/data/2.5/onecall?lat=${cityWeatherInfo.coord.lat}&lon=${cityWeatherInfo.coord.lon}&units=metric&
+    exclude=hourly,minutely&appid=e4dfa41e22f93aa72d2e80838a3bb930`
+
+    const resDaily = await fetch(url2);
+    cityWeatherInfoDaily = await resDaily.json();
+
+    console.log(cityWeatherInfo);
+    console.log(cityWeatherInfoDaily);
+    paintData();
+
+}
+
+const paintData = () => {
+    //Genral Section
+    paintGeneral();
+    //Daily
+    paintDaily();
+
+}
+
+const paintGeneral = () => {
+    //console.log(cityWeatherInfo.base);
+    let nameCity = cityWeatherInfo.name
+    let date = dateNow.toDateString();
+    let icon = cityWeatherInfo.weather[0].icon;
+    let temp = Math.floor(cityWeatherInfo.main.temp - 273.15)
+    let tempMin = Math.floor(cityWeatherInfo.main.temp_min - 273.15)
+    let tempMax = Math.floor(cityWeatherInfo.main.temp_max - 273.15)
+    let feelsLike = Math.floor(cityWeatherInfo.main.feels_like - 273.15)
+
+    //New Elements
+    let image = document.createElement("img");
+    let temperature = document.createElement("h2");
+    let descResume = document.createElement("small");
+
+    document.getElementById("cityName").innerText = nameCity;
+    document.getElementById("date").innerHTML = `${date}<br>${cityWeatherInfo.weather[0].main}`;
+
+    image.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    document.getElementById("picture").appendChild(image);
+
+    temperature.textContent = `${temp}°C`
+    document.getElementById("temp").appendChild(temperature);
+
+    descResume.innerHTML = `${tempMax}°/${tempMin}°  Feels like ${feelsLike}° <br> ${cityWeatherInfo.weather[0].description}`
+    document.getElementById("descInfo").appendChild(descResume);
+}
+
+const paintDaily = () => {
+
+    let dailyArray = cityWeatherInfoDaily.daily;
+    let detailTable = document.getElementById("detailTable")
+    let nameDays = ["Mon", "Sun", "Tue", "Wen", "Thu", "Fri", "Sat"];
+    // let day = new Date(dateNow);
+
+
+    dailyArray.forEach((element, index) => {
+        let day = new Date(dateNow);
+        day.setDate(day.getDate() + index);
+        // day = new Date(dateNow.setDate(dateNow.getDate() + index));
+
+        let iconImg = `http://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png}`
+        // <img src="http://openweathermap.org/img/wn/02d@2x.png" class="max-width: 50%;" >
+        // <img src="http://openweathermap.org/img/wn/${iconImg}@2x.png">
+
+        detailTable.innerHTML += `<tr class="table-warning">                               
+            <td>${nameDays[day.getDay()]}</td>
+            <td>${element.temp.min}°c</td>
+            <td>${element.temp.max}°c</td>
+          </tr> `
+
+    });
+
+    console.table(dailyArray)
+
+}
+
+getWeatherData();
--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const search = document.getElementById("searchCity");
 const matchList = document.getElementById("match-list");
+const setCards = document.querySelector("#match-list");
+const btnSetCity = document.getElementById("btnSetCity");
+
+var citySelected = {};
+var matches = [];
+
 
 //Search cities
 const searchCities = async searchText => {
-    const res = await fetch("../data/city.list.json");
+    const res = await fetch("../data/city.list.min.json");
     const cities = await res.json();
 
     //Get matches to current input
-    let matches = cities.filter(city => {
+    matches = cities.filter(city => {
         const regex = new RegExp(`^${searchText}`, 'gi')
         return city.name.match(regex) || city.country.match(regex)
     });
@@ -16,18 +22,48 @@ const searchCities = async searchText => {
         matches = [];
         matchList.innerHTML = "";
     }
-
+    console.log(matches)
     outputHtml(matches);
 }
 
 //results in html
 const outputHtml = matches => {
     if (matches.length > 0) {
-        const html = matches.map(match => `<div class="card card-body mb-1">
-        <h4>${match.name} (${match.country}) <span class="text-primary">${match.name}</span></h4>
-        <small>Lat: ${match.coord.lat} / Long: ${match.coord.lon}`);
+        const html = matches.map(match => `<div class="card card-body mb-1" id="d${match.id}">
+        <h4 id="h${match.id}">${match.name} (${match.country}) <span id="s${match.id}" class="text-primary">${match.state}</span></h4>
+        <small id="m${match.id}" >Lat: ${match.coord.lat} / Long: ${match.coord.lon}</small></div>`).join('');
         matchList.innerHTML = html;
     }
 }
 
 search.addEventListener('input', () => searchCities(search.value));
+
+setCards.addEventListener('click', e => {
+
+    if (e.target.id.length > 0 && e.target.id != "match-list") {
+
+        search.setAttribute("id_city", e.target.id.substring(1, e.target.id.length))
+
+        citySelected = matches.find(city => { return city.id == parseInt(search.getAttribute("id_city")) })
+        search.value = `${citySelected.name} (${citySelected.country}) ${citySelected.state} `
+        matchList.innerHTML = "";
+
+        search.setAttribute("name_city", citySelected.name)
+        search.setAttribute("lat_city", citySelected.coord.lat)
+        search.setAttribute("lon_city", citySelected.coord.lon)
+
+        //Saving the local city
+        localStorage.setItem("id_localCity", search.getAttribute("id_city"))
+        localStorage.setItem("name_localCity", search.getAttribute("name_city"))
+        localStorage.setItem("lat_localCity", search.getAttribute("lat_city"))
+        localStorage.setItem("lon_localCity", search.getAttribute("lon_city"))
+
+    }
+
+});
+
+btnSetCity.addEventListener('click', () => {
+
+    window.open("info.html", "_self");
+
+})
